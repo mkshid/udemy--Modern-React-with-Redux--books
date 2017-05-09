449e6565213d08971ab958554d1dfe9f85f46921
M. Hassan Khurshid <mkshid@gmail.com>
2017-05-09 17:45:56 +0200

    Add pages

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    {title: 'Javascripts the goods parts'},
-    {title: 'Harry Potter'},
-    {title: 'The dark tower'},
-    {title: 'Eloquent Ruby'}
+    {title: 'Javascripts the goods parts', pages: 100},
+    {title: 'Harry Potter', pages: 600},
+    {title: 'The dark tower', pages: 1200},
+    {title: 'Eloquent Ruby', pages: 80}
   ];
 
 }
