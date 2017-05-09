821663321587e6c7b7ee8815d594c6287d683881
M. Hassan Khurshid <mkshid@gmail.com>
2017-05-09 12:46:39 +0200

    Add books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: 'Javascripts the goods parts'},
+    {title: 'Harry Potter'},
+    {title: 'The dark tower'},
+    {title: 'Eloquent Ruby'}
+  ];
+
+}
